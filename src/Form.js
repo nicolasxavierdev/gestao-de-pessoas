8ded835108111512cb981896bc7d9208b3b19cfc
nicolasxavierdev <nicolasxavier.dev@gmail.com>
2021-06-11 15:45:38 -0300

    formulário

--- src/Form.js
+++ src/Form.js
@@ -5,64 +5,43 @@ export default class Form extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            form: {
+            form:{
                 nome: '',
                 email: '',
                 cpf: '',
-                dataDeNascimento: '',
-                error: ''
+                dateOfBirth: ''
             }
         }
 
         this.dadosForm = this.dadosForm.bind(this)
-        this.cadastrar = this.cadastrar.bind(this)
 
     }
 
-    dadosForm(e) {
+    dadosForm(e){
         let form = this.state.form
         form[e.target.name] = e.target.value
         this.setState({form: form})
     }
 
-    cadastrar(e) {
-        const { nome, email, cpf, dataDeNascimento } = this.state
-
-        if (nome !== '' && email !== '' && cpf !== '' && dataDeNascimento !== '') {
-            alert(`Nome: ${nome} \nE-mail: ${email} \nCPF: ${cpf} \nData de Nascimento: ${dataDeNascimento}`)
-        } else {
-            this.setState({ error: 'Ops, Parece que está faltando algo!' })
-        }
-
-        e.preventDefault()
-    }
-
     render() {
         return (
             <div>
-                <h1>Formulário.</h1>
-
-                {this.state.error && <p>{this.state.error}</p>}
-                
-                <form onSubmit={this.cadastrar}>
-                    
-                    <label>Nome:</label>
-                    <input type='text' value={this.state.form.nome} onChange={this.dadosForm} />
-                    
-                    <label>E-mail:</label>
-                    <input type='email' value={this.state.form.email} onChange={this.dadosForm} />
-                    
-                    <label>CPF:</label>
-                    <input type='text' value={this.state.form.cpf} onChange={this.dadosForm} />
-                    
-                    <label>Data de Nascimento:</label>
-                    <input type='date' value={this.state.form.dataDeNascimento} onChange={this.dadosForm} />
-                    
-                    <button type='submit'>Enviar</button>
-                </form>
+                <h1>Formulário</h1>
+                Nome:
+                <input type='text' name='nome' value={this.state.form.nome} onChange={this.dadosForm} />
+                E-mail:
+                <input type='email' name='email' value={this.state.form.email} onChange={this.dadosForm} />
+                CPF:
+                <input type='text' name='cpf' value={this.state.form.cpf} onChange={this.dadosForm} />
+                Data de Nascimento:
+                <input type='date' name='date'  onChange={this.dadosForm} />
+                <div>
+                    <h3> {this.state.form.name} </h3>
+                    <h3> {this.state.form.email} </h3>
+                    <h3> {this.state.form.cpf} </h3>
+                    <h3> {this.state.form.dateOfBirth} </h3>
+                </div>
             </div>
         )
     }
 }
-
-
