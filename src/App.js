6ee556632736c59b346b4478389cb9a1ba49a83f
nicolasxavierdev <nicolasxavier.dev@gmail.com>
2021-06-22 10:03:00 -0300

    Componente para componente

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-import Form from './components/Form';
-import './App.css';
+import React, { Component } from 'react'
+import Form from './components/Form'
 
-function App() {
-  return (
-    <div className="App">
-      <Form/>
-    </div>
-  );
+export default class App extends Component {
+
+    // eslint-disable-next-line no-useless-constructor
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            nome: '',
+            email: '',
+            cpf: '',
+            dateOfBirth: ''
+        }
+    }
+
+    render() {
+
+        return (
+            <div>
+                <h1>Formulário</h1>
+
+                <div>
+                    <label>Nome:</label> <br />
+                    <input type='text' value={this.state.nome} onChange={(e) => this.setState({ nome: e.target.value })} />
+                </div>
+
+                <div>
+                    <label>E-mail:</label> <br />
+                    <input type='email' value={this.state.email} onChange={(e) => this.setState({ email: e.target.value })} />
+                </div>
+
+                <div>
+                    <label>CPF:</label> <br />
+                    <input type='text' value={this.state.cpf} onChange={(e) => this.setState({ cpf: e.target.value })} />
+                </div>
+
+                <div>
+                    <label>Data de Nascimento:</label> <br />
+                    <input type='date' onChange={(e) => this.setState({ dateOfBirth: e.target.value })} />
+                </div>
+                <hr/>
+                <Form dados={this.state} />
+            </div>
+        );
+    }
+
 }
-
-export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,52 +1,24 @@
 import React, { Component } from 'react'
 
-export default class Form extends Component {
+class Form extends Component {
 
+    // eslint-disable-next-line no-useless-constructor
     constructor(props) {
         super(props)
-        this.state = {
-            form:{
-                nome: '',
-                email: '',
-                cpf: '',
-                dateOfBirth: ''
-            }
-        }
-
-        this.dadosForm = this.dadosForm.bind(this)
-
     }
 
-    dadosForm(e){
-        let form = this.state.form
-        form[e.target.name] = e.target.value
-        this.setState({form: form})
-    }
 
     render() {
+        let { nome, email, cpf, dateOfBirth } = this.props.dados
         return (
             <div>
-                <h1>Formulário</h1>
-
-                Nome:
-                <input type='text' name='nome' value={this.state.form.nome} onChange={this.dadosForm} />
-                
-                E-mail:
-                <input type='email' name='email' value={this.state.form.email} onChange={this.dadosForm} />
-                
-                CPF:
-                <input type='text' name='cpf' value={this.state.form.cpf} onChange={this.dadosForm} />
-                
-                Data de Nascimento:
-                <input type='date' name='dateOfBirth' onChange={this.dadosForm} />
-                
-                <div>
-                    <h3> {this.state.form.nome} </h3>
-                    <h3> {this.state.form.email} </h3>
-                    <h3> {this.state.form.cpf} </h3>
-                    <h3> {this.state.form.dateOfBirth} </h3>
-                </div>
+                <h3>Nome: {nome} </h3>
+                <h3>E-mail: {email} </h3>
+                <h3>CPF: {cpf} </h3>
+                <h3>Data de Nascimento: {dateOfBirth} </h3>
             </div>
         )
     }
 }
+
+export default Form;
